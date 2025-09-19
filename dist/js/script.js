5140f9ff774cf83a0c2aacd205c9aa2893736902
Risma Putri <wirdianthi05@gmail.com>
2025-09-19 21:46:30 +0800

    third commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,6 @@
 const scrollUp = document.querySelector('.scroll-up');
 window.onscroll = () => {
-    if (window.scrollY > 500) {
+    if (window.scrollY > 600) {
         scrollUp.classList.add("scroll-active");
     } else {
         scrollUp.classList.remove("scroll-active");
